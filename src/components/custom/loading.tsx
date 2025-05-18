d01cb708d91b87dfcb1109e56d5ba56157f65752
jisuzzzzz <meked@naver.com>
2025-05-18 12:14:16 +0900

    chore: reduce loading component size for better visual balance

--- src/components/custom/loading.tsx
+++ src/components/custom/loading.tsx
@@ -21,7 +21,7 @@ export default function Loading() {
         animate="animate"
         variants={spinAnimation}
       >
-        <svg width="180" height="180" viewBox="0 0 60 60" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg width="130" height="130" viewBox="0 0 60 60" fill="none" xmlns="http://www.w3.org/2000/svg">
           <defs>
             <linearGradient id="cloudGradient" x1="0%" y1="0%" x2="100%" y2="100%" gradientUnits="userSpaceOnUse">
               <stop offset="0%" stopColor="#FFFFFF">
